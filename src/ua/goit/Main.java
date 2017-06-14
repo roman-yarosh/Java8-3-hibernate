3178b8f2fd4627b7edb0f6f4db9bcec63884471f
roman-yarosh <yracompany@gmail.com>
2017-06-14 15:53:03 +0300

    Commit project

---
+++ src/ua/goit/Main.java
@@ -0,0 +1,12 @@
+package ua.goit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+
+    }
+}
