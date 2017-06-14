8de608ad47fdaa0278e4c79924732e7b5ca924d2
roman-yarosh <yracompany@gmail.com>
2017-06-14 16:19:36 +0300

    Commit

--- src/ua/goit/Main.java
+++ src/ua/goit/Main.java
@@ -3,13 +3,6 @@ package ua.goit;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-
+        
     }
 }
