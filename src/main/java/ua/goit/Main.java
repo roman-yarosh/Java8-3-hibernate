648fd17c022b3abf6e218c963cfe01c729754e12
roman-yarosh <yracompany@gmail.com>
2017-06-15 17:51:00 +0300

    JDBC Developer DAO

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -19,14 +19,17 @@ public class Main {
         do {
             System.out.println("Select entity: 1 - Developer, 0 - Exit:");
             inputEntity = in.nextInt();
+            //правильную обработку ввода если не целое
             System.out.println("Selected entity: " + inputEntity);
             if (inputEntity > 0) do {
                 System.out.println("Select CRUD operation: 1 - Create, 2 - Read, 3 - Update, 4 - Delete, 0 - Exit:");
                 inputCRUD = in.nextInt();
                 System.out.println("You selected entity: " + inputEntity + " and operation: " + inputCRUD);
-            } while (inputCRUD > 0);
+                scannDeveloper();
+                System.out.println("You selected entity: " + inputEntity + " and operation: " + inputCRUD);
 
+
+            } while (inputCRUD > 0);
         } while (inputEntity > 0);
-        1
     }
 }
