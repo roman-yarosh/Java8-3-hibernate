8132a53f0cc99acd4c8b524e13eb8ea1e0eecc3d
roman-yarosh <yracompany@gmail.com>
2017-06-15 15:16:04 +0300

    JDBC Developer DAO

--- src/ua/goit/controller/DevelopersController.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.controller;
-
-/**
- * Created by yarosh_ra on 14.06.2017.
- */
-public class DevelopersController {
-}
--- src/ua/goit/model/dao/DeveloperDAO.java
+++
@@ -1,6 +0,0 @@
-package ua.goit.model.dao;
-
-import ua.goit.model.entity.Developer;
-
-public interface DeveloperDAO extends GenericDao<String, Developer> {
-}
