1ab0861d859fc9a6d6bd77a214e071105994d70d
Roman Yarosh <yracompany@gmail.com>
2017-06-16 22:46:21 +0300

    JDBC Developer DAO

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -10,14 +10,10 @@ public class Main {
     public static void main(String[] args) {
 
         DeveloperDAO developerDAO = new JdbcDeveloperDAO();
-        //System.out.println(developerDAO.read(Long.valueOf(1)));
+        System.out.println(developerDAO.read(Long.valueOf(1)));
         developerDAO.getAllDevelopers().forEach(System.out::println);
-
-
-
-//        System.exit(0);
-
         showCrudDialod();
+//        System.exit(0);
     }
 
 
